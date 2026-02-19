fc66c7905a58d53b4f164237d29ca2383f4f258c
Zenith Kandel <kandelze123@gmail.com>
2026-02-19 10:50:25 +0545

    Refactor SQL dump for test_sawari_db: streamline structure and remove obsolete file

--- schema.sql
+++ schema.sql
@@ -1,8 +1,25 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 19, 2026 at 06:04 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `test_sawari_db`
 --
-CREATE DATABASE IF NOT EXISTS `test_sawari_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `test_sawari_db`;
 
 -- --------------------------------------------------------
 
